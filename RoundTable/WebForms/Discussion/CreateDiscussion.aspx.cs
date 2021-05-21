5ab27917bc2905cf34791d9df2fa0e2ff6bc0b7e
User <nicholaslhq-wp18@student.tarc.edu.my>
2021-05-21 16:31:24 +0800

    CreateDiscussion.aspx

--- RoundTable/WebForms/Discussion/CreateDiscussion.aspx.cs
+++ RoundTable/WebForms/Discussion/CreateDiscussion.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace RoundTable.WebForms.Discussion
 {
@@ -11,7 +13,54 @@ namespace RoundTable.WebForms.Discussion
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(!Page.IsPostBack)
+            {
+                SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\RoundTableDB.mdf;Integrated Security=True");
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Topic", con);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                DropDownList1.DataSource = dt;
+                DropDownList1.DataBind();
 
+                DropDownList1.Items.Insert(0, "[Select a Topic]");
+                DropDownList2.Items.Insert(0, "[Select a Tag]");
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DropDownList2.Enabled = true;
+            DropDownList1.Items.Remove(DropDownList1.Items.FindByText("[Select a Topic]"));
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\RoundTableDB.mdf;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("SELECT topicID FROM Topic WHERE topicName='" + DropDownList1.SelectedItem.Value + "'", con);
+            con.Open();
+            String topicID = cmd.ExecuteScalar().ToString();
+            con.Close();
+
+            SqlCommand cmd2 = new SqlCommand("SELECT * FROM Tag WHERE topicID='" + topicID + "'", con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd2);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            DropDownList2.DataSource = dt;
+            DropDownList2.DataBind();
+            DropDownList2.Items.Insert(0, "[Select a Tag]");
+        }
+
+        protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DropDownList2.Items.Remove(DropDownList2.Items.FindByText("[Select a Tag]"));
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DropDownList1.ClearSelection();
+            DropDownList2.ClearSelection();
+            DropDownList1.Items.Insert(0, "[Select a Topic]");
+            DropDownList2.Items.Insert(0, "[Select a Tag]");
+
+            TextBox1.Text = "";
+            TextBox2.Text = "";
         }
     }
 }

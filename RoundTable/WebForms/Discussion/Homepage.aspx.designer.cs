31fbd30a616bdc52e3d1abd61d03eb93f35b0101
User <nicholaslhq-wp18@student.tarc.edu.my>
2021-05-29 18:01:32 +0800

    Modified DB and Homepage. Added Topic and Tag.

--- RoundTable/WebForms/Discussion/Homepage.aspx.designer.cs
+++ RoundTable/WebForms/Discussion/Homepage.aspx.designer.cs
@@ -13,5 +13,23 @@ namespace RoundTable.WebForms.Discussion
 
     public partial class Homepage
     {
+
+        /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+
+        /// <summary>
+        /// Repeater1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater Repeater1;
     }
 }
